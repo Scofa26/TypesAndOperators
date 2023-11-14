dcc86f5dd004c82aba9d527983173a14cadbdc7d
Sophia <chida2001@mail.ru>
2023-11-14 19:47:41 +0300

    -m 'initial com'

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TypesAndOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Enter name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello user, {name}");
+           */
+
+           bool isWhiteBreadFreash = true;
+           ushort whiteBreadPrice = 150;
+           ushort butterPrice= 230;
+           ushort milkPrice = 170;
+           float milkFat = 1.2F;
+           
+        }
+    }
+}
